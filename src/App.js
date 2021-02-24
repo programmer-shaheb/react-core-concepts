ac7fcc10410ed54b9e2e94da6f6c870a7b2d3a8d
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-02-24 13:05:20 +0600

    file updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Welcome(props) {
+  return (
+    <div>
+      <h1>Hello,{props.name}</h1>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img
+      src="{props.user.avatarUrl}"
+      alt="{props.user.name}"
+      className="Avatar"
+    />
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Welcome name="Abidur"></Welcome>
     </div>
   );
 }
