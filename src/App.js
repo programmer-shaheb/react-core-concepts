04bebd69957e6821c4ee4aefd099c1700915c8aa
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-02-24 14:53:59 +0600

    File Updated

--- src/App.js
+++ src/App.js
@@ -19,15 +19,21 @@ function Avatar(props) {
   );
 }
 
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.author} />
+      <div className="UserInfo-name">{props.author.name}</div>
+    </div>
+  );
+}
+
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <Avatar user={props.author} />
-        <div className="UserInfo-name">{props.author.name}</div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">{formatDate(props.date)}</div>
+      <div className="Comment-date">{props.date}</div>
     </div>
   );
 }
@@ -35,7 +41,8 @@ function Comment(props) {
 function App() {
   return (
     <div>
-      <Welcome name="Abidur"></Welcome>
+      <Welcome name="Akash"></Welcome>
+      <Comment author="Abidur" text="Read The Book" date={new Date()}></Comment>
     </div>
   );
 }
