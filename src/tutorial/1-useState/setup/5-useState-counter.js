e70e0702a4ece87fbb17bfe0c6f31eeeb0ca2bad
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-03-07 23:03:50 +0600

    file updated

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h1>Simple Counter</h1>
+        <h2>{value}</h2>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,75 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const handlerBtn = (e) => {
+    e.preventDefault();
+    if (name && email) {
+      // Below part is an object. If the property & value name are same,we can use like this
+      const person = { name, email };
+      // Above part .......
+      setPeople([...people, person]);
+      setName("");
+      setEmail("");
+    } else {
+      alert("Fill The Form");
+    }
+  };
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handlerBtn}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              id="firstName"
+              name="firstName"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              value={email}
+              id="email"
+              name="email"
+            />
+          </div>
+          {/* <div className="form-control">
+            <label htmlFor="password">Password : </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              value={password}
+              id="password"
+              name="password"
+            />
+          </div> */}
+          <button type="text">Add Me</button>
+        </form>
+        {people.map((person) => {
+          const { name, email } = person;
+          return (
+            <>
+              <div className="item">
+                <h5>{name}</h5>
+                <p>{email}</p>
+                <button className="btn">Remove</button>
+              </div>
+            </>
+          );
+        })}
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
